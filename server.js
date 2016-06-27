eb6f16c76fdaf96f62fc0b28456133b4dfc7cead
Corey Ghilarducci <cghilarducci@gmail.com>
2016-06-27 11:30:58 -0700

    fixed gitignore file

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express'),
     jwt = require('jsonwebtoken'),
     fs = require('fs'),
     request = require('request'),
-	crypto = require('crypto'),
+    crypto = require('crypto'),
     colors = require('colors'),
     argv = require('minimist')(process.argv.slice(2));
 
@@ -17,19 +17,19 @@ if (argv.user_id != undefined || argv.U != undefined) {
 }
 
 if ((argv.api_key || argv.K) && (user_id || enterprise_id) && (argv.passphrase || argv.P) && (argv.client_secret || argv.S)) {
-	
+
     var box_sub_type;
 
-    if (user_id != undefined){
+    if (user_id != undefined) {
         box_sub_type = "user";
     } else {
         box_sub_type = "enterprise";
     }
 
-	var api_key = argv.api_key || argv.K,
-		ent_or_user_id = user_id || enterprise_id,
-		passphrase = argv.passphrase || argv.P,
-		client_secret = argv.client_secret || argv.S;
+    var api_key = argv.api_key || argv.K,
+        ent_or_user_id = user_id || enterprise_id,
+        passphrase = argv.passphrase || argv.P,
+        client_secret = argv.client_secret || argv.S;
 
     var privateKey = fs.readFileSync('private_key.pem');
     var publicKey = fs.readFileSync('public_key.pem');
@@ -38,30 +38,30 @@ if ((argv.api_key || argv.K) && (user_id || enterprise_id) && (argv.passphrase |
     console.info('Access token is only good for 60 mins');
 
 } else if (argv.H || argv.help) {
-	console.log(' ');
-	console.log('This tool uses Box JWTs to ' + 'create access tokens for App Auth.');
-	console.log(' ')
-	console.log('These are the following options.'.underline);
-	console.log('-K or --api_key'.blue + ' : API key for Box Platform');
-	console.log('-E or --enterprise_id'.blue + ' : ID of the enterprise');
+    console.log(' ');
+    console.log('This tool uses Box JWTs to ' + 'create access tokens for App Auth.');
+    console.log(' ')
+    console.log('These are the following options.'.underline);
+    console.log('-K or --api_key'.blue + ' : API key for Box Platform');
+    console.log('-E or --enterprise_id'.blue + ' : ID of the enterprise');
     console.log('-U or --user_id'.blue + ' : ID of the App User');
-	console.log('-P or --passphrase'.blue + ' : secret for the JWT signing. Must match PEM');
-	console.log('-S or --client_secret'.blue + ': found in the developer console');
+    console.log('-P or --passphrase'.blue + ' : secret for the JWT signing. Must match PEM');
+    console.log('-S or --client_secret'.blue + ': found in the developer console');
     console.log(' ');
 } else {
     console.log(' ');
-    console.log('Unable to generate token. '.red + 'Please check arguements.') 
+    console.log('Unable to generate token. '.red + 'Please check arguements.')
     console.log('Use --help or -H for instructions');
     console.log(' ');
 }
 
 // jwt_secret will be the passphrase from above
 function generateToken(API_token, ent_or_user_id, box_sub_type, jwt_secret, client_secret) {
-     var API_token = API_token,
-     	ent_or_user_id = ent_or_user_id,
-     	jwt_secret = jwt_secret,
-     	client_secret = client_secret
-        box_sub_type = box_sub_type;
+    var API_token = API_token,
+        ent_or_user_id = ent_or_user_id,
+        jwt_secret = jwt_secret,
+        client_secret = client_secret
+    box_sub_type = box_sub_type;
 
     ent_or_user_id = ent_or_user_id.toString();
     var expiringTime = generateExpiringTime();
@@ -76,7 +76,8 @@ function generateToken(API_token, ent_or_user_id, box_sub_type, jwt_secret, clie
         jti: sessionToken,
         exp: expiringTime
     }, { key: privateKey, passphrase: jwt_secret }, { algorithm: 'RS256', noTimestamp: true });
-
+    console.log(signed_token);
+    console.log("------------------------");
     var boxResponse = requestForAccesToken(signed_token, client_secret, API_token);
 };
 
@@ -95,14 +96,14 @@ function requestForAccesToken(signed_token, client_secret, API_token) {
         }
     };
 
-	request(options, function(error, response, body){
-    	if (error) throw new Error(error);
-    	var body = JSON.parse(body);
-    	var accessToken = body.access_token;
+    request(options, function(error, response, body) {
+        if (error) throw new Error(error);
+        var body = JSON.parse(body);
+        var accessToken = body.access_token;
         console.log(body);
-    	console.log(' ');
-    	console.log(colors.red('Access Token : %s'), accessToken);
-    	console.log(' ');
+        console.log(' ');
+        console.log(colors.red('Access Token : %s'), accessToken);
+        console.log(' ');
     });
 
 };
